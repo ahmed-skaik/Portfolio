245c8bde4dd6f15bab4e5b1720a856c5e74b6078
Ahmed skaik <ahmedskaik.2001@gmail.com>
2021-11-02 23:11:03 +0200

    Adding JS animation

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -10,6 +10,16 @@ window.onscroll = function () {
   }else {
     btn.style.display  = "none"
   }
+// skills sectoin
+  if (window.scrollY >= sectionTop.offsetTop - 100 &&  window.scrollY <= sectionBot.offsetTop - 200) {
+    spanProg.forEach ( (span) => {
+      span.style.width = span.dataset.width;
+    })
+  }else {
+    spanProg.forEach ( (span) => {
+      span.style.width = 0;
+    })
+  }
 }
 
 btn.onclick = function() {
@@ -19,3 +29,21 @@ btn.onclick = function() {
   })
 }
 // scroll up end
+
+
+let sectionTop = document.querySelector(".skills")
+let sectionBot = document.querySelector("footer")
+let spanProg = document.querySelectorAll(".skills .skills-stat .prog-holder .prog span")
+
+// window.onscroll = function () {
+//   if (window.scrollY >= sectionTop.offsetTop - 100 &&  window.scrollY <= sectionBot.offsetTop - 200) {
+//     spanProg.forEach ( (span) => {
+//       span.style.width = span.dataset.width;
+//     })
+//   }else {
+//     spanProg.forEach ( (span) => {
+//       span.style.width = 0;
+//     })
+//   }
+// }
+// skills start
