ee8f6b7de2063ed73c2bbfb63e5456acce78c99d
Ahmed skaik <ahmedskaik.2001@gmail.com>
2021-11-15 00:27:12 +0200

    inhancing JS code syntax

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -4,13 +4,31 @@ console.log(welcomeMsg)
 // scroll up start
 let btn = document.getElementById("btn")
 
-window.onscroll = function () {
+btn.onclick = function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+
+function scroll() {
   if (scrollY >= 250) {
     btn.style.display = "block"
   }else {
     btn.style.display  = "none"
   }
-// skills sectoin
+}
+
+window.addEventListener("scroll", scroll)
+
+// scroll up end
+
+// skills start
+let sectionTop = document.querySelector(".skills")
+let sectionBot = document.querySelector("footer")
+let spanProg = document.querySelectorAll(".skills .skills-stat .prog-holder .prog span")
+
+function skills() {
   if (window.scrollY >= sectionTop.offsetTop - 100 &&  window.scrollY <= sectionBot.offsetTop - 200) {
     spanProg.forEach ( (span) => {
       span.style.width = span.dataset.width;
@@ -22,28 +40,5 @@ window.onscroll = function () {
   }
 }
 
-btn.onclick = function() {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  })
-}
-// scroll up end
-
-
-let sectionTop = document.querySelector(".skills")
-let sectionBot = document.querySelector("footer")
-let spanProg = document.querySelectorAll(".skills .skills-stat .prog-holder .prog span")
-
-// window.onscroll = function () {
-//   if (window.scrollY >= sectionTop.offsetTop - 100 &&  window.scrollY <= sectionBot.offsetTop - 200) {
-//     spanProg.forEach ( (span) => {
-//       span.style.width = span.dataset.width;
-//     })
-//   }else {
-//     spanProg.forEach ( (span) => {
-//       span.style.width = 0;
-//     })
-//   }
-// }
-// skills start
+window.addEventListener("scroll", skills)
+// skills end
